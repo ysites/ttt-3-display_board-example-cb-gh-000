f265a93632f740c37dbb8a9d53a41f56c16d4aaa
Yadiel Cabrera <ycabr035@fiu.edu>
2017-11-10 01:25:58 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
+
+board = [" "]
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  prints "  ""|""  "|"" 
+  prints "-----------"
+  prints "  |  |  "
+  prints "-----------"
+  prints "  |  |  "
+end
